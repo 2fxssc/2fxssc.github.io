fc002f7a6a6676096dc524f354486b6eb070ccf4
Shuzhou Liu <2fxclass2@cc.edu.rs>
2020-10-29 21:09:03 +0800

    Update login.js

--- login.js
+++ login.js
@@ -5,38 +5,60 @@ function login(){
     if (y.value == "20200223"){
       localStorage.setItem("userName", x.value);
       localStorage.setItem("isLogin", true);
+      window.location.href="https://2fxssc.github.io/member/center/";
     }
+    else alert("对不起，您的密码有误，请再次输入！");
   }
   else {
     if (x.value == "shuzhouliu"){
      if (y.value == "20200207"){
         localStorage.setItem("userName", x.value);
         localStorage.setItem("isLogin", true);
-      }
+       window.location.href="https://2fxssc.github.io/member/center/";
+        
+      }else alert("对不起，您的密码有误，请再次输入！");
     }
     else {
     if (x.value == "xuan"){
      if (y.value == "20200222"){
         localStorage.setItem("userName", x.value);
         localStorage.setItem("isLogin", true);
+       window.location.href="https://2fxssc.github.io/member/center/";
       }
+       else alert("对不起，您的密码有误，请再次输入！");
     }
       else {
     if (x.value == "jimmymeng"){
      if (y.value == "20200208"){
         localStorage.setItem("userName", x.value);
         localStorage.setItem("isLogin", true);
+       window.location.href="https://2fxssc.github.io/member/center/";
       }
+       else alert("对不起，您的密码有误，请再次输入！");
     }
         else {
     if (x.value == "yzy"){
      if (y.value == "20200236"){
         localStorage.setItem("userName", x.value);
         localStorage.setItem("isLogin", true);
+       window.location.href="https://2fxssc.github.io/member/center/";
       }
+       else alert("对不起，您的密码有误，请再次输入！");
     }
   }
+        else {
+         if (x.value == "huazihao"){
+          if (y.value== "20200204"){
+            localStorage.setItem("userName", x.value);
+          localStorage.setItem("isLogin", true);
+            window.location.href="https://2fxssc.github.io/member/center/";
+          }
+         }
+          else alert("对不起，您的密码有误，请再次输入！");
+        }
   }
   }
   }
-}
+  else{
+   alert("对不起，您的用户名有误，很可能你不是申诉处成员，谢谢！"); 
+  }
